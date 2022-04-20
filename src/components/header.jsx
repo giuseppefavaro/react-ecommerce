88107b1dad17a41f3c71fa3c529650039951e2ea
Giuseppe <giuseppefvr@gmail.com>
2022-04-20 15:23:29 +0200

    feat: added 4 components

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+    return (
+        <header>
+        <img src="" alt="React ecommerce" />
+        <form>
+            <input type="text" placeholder="Search..." />
+        </form>
+        </header>
+    )
+}
+
+export {Header};
---
+++ src/components/product.jsx
@@ -0,0 +1,11 @@
+const Product = () => {
+    return  (
+        <article>
+            <img src="" alt="" />
+            <p>Prezzo: 1000€</p>
+            <h4>Prodotto mittticco</h4>
+        </article>
+    )
+}
+
+export {Product};
