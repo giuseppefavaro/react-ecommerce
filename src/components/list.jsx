6788e93ac0477851c42f9cc0f6b9ae5f00b5b5bf
Giuseppe <giuseppefvr@gmail.com>
2022-04-21 15:19:14 +0200

    feat: initial product loading

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,20 +1,40 @@
 import { useEffect, useState } from "react";
-
 import { Product } from "./product";
 
-const initialProducts = ["prodotto 1", "prodotto 2", "prodotto 3"];
+const mock = [
+  {
+    id: 0,
+    title: "prodotto 1",
+    price: 0,
+    image: "https://via.placeholder.com/500x300",
+  },
+  {
+    id: 1,
+    title: "prodotto 2",
+    price: 0,
+    image: "https://via.placeholder.com/500x300",
+  },
+  {
+    id: 2,
+    title: "prodotto 3",
+    price: 0,
+    image: "https://via.placeholder.com/500x300",
+  },
+];
 
 export const List = () => {
 
-    const [products, setProducts] = useState(initialProducts);
+    const [products, setProducts] = useState(mock);
+    const [isLoading, setLoading] = useState(true);
 
     const getData = async () => {
         const response = await fetch("https://fakestoreapi.com/products");
         const data = await response.json();
         setProducts(data);
+        setLoading(false);
     };
 
-    console.log(products);
+    // console.log(products);
 
 
     useEffect(() => { getData() }, [])
@@ -24,9 +44,9 @@ export const List = () => {
   return (
     <section className="products">
       <ul className="productsGrid">
-        {products.map((item, index) => (
-          <li key={index}>
-            <Product img={item.image} title={item.title} price={item.price} />
+        {products.map((item) => (
+          <li key={item.id} className={isLoading ? "loading" : ""}>
+            <Product image={item.image} name={item.title} price={item.price} />
           </li>
         ))}
       </ul>
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,9 +1,14 @@
 const Product = (props) => {
+
+    const name = props.name || "Product name";
+    const price = props.price || 100;
+    const image = props.image || "";
+
     return  (
         <article className="product">
-            <img src={props.img} alt="" />
-            <p>{props.price}</p>
-            <h4>{props.title}</h4>
+            <img src={image} alt={name} />
+            <p>{price}</p>
+            <h4>{name}</h4>
         </article>
     )
 }
